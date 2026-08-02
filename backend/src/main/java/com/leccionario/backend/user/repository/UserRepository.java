9fec0dcb29b7d6a886e889397f515869088fd982
ortizal <alexis.ortiz81@outlook.com>
2026-08-02 17:25:10 -0500

    update users

--- backend/src/main/java/com/leccionario/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/leccionario/backend/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.leccionario.backend.user.repository;
 
 import com.leccionario.backend.user.domain.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = {"institution", "roles", "roles.permissions"})
     Optional<User> findByUsername(String username);
 
     Optional<User> findByUsernameIgnoreCase(String username);
